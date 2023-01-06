d9b339b68f47ae7c55946142b58efced61642d81
aymane-smi <aymanebel2@outlook.fr>
2023-01-06 15:05:40 +0100

    path sums

---
+++ DSA-solutions/tree/path-sum/solutions.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if(root === null)
+        return false;
+    if(root.left === null && root.right === null && targetSum - root.val === 0)
+        return true;
+    return hasPathSum(root.right, targetSum-root.val) || hasPathSum(root.left, targetSum-root.val);
+};
