c308dc8c25dd933295da6a4ea2db0fe4d200626e
aymane-smi <aymanebel2@outlook.fr>
2023-02-03 10:23:22 +0100

    best time to but and sell stock

--- DSA-solutions/number/best-time-to-buy-and-sell-stock/solution.java
+++ DSA-solutions/number/best-time-to-buy-and-sell-stock/solution.java
@@ -1,15 +1,14 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int r = 1, l = 0, maxP = 0;
-        while(r < prices.size()){
+    public int maxProfit(int[] prices) {
+        int r = 0, l =0, maxP = 0;
+        while(r < prices.length){
             int profit = prices[r] - prices[l];
             if(profit > 0)
-                maxP = maxP > profit ? maxP : profit;
+                maxP = Math.max(profit, maxP);
             else
                 l = r;
             r++;
         }
         return maxP;
     }
-};
+}
